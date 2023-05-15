8fd4bfe21ff2e8657dd77a1c634cd3b6caeab004
Jochen Görtler <me@jgoertler.com>
2023-05-15 12:14:48 +0200

    ignore `build/`

--- build/_app/immutable/chunks/0.a74ac370.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/layout.svelte.d0f1c823.js";export{t as component};
--- build/_app/immutable/chunks/1.fb7800c8.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.1cb6e4b6.js";export{t as component};
