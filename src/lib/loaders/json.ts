2ea43bf3a803b5305db42d99323f5a88fea7aae8
Dominik Moritz <domoritz@gmail.com>
2023-05-16 21:36:24 +0200

    fix: hacky way to fix base url issue

--- src/lib/loaders/json.ts
+++ src/lib/loaders/json.ts
@@ -8,7 +8,8 @@ import type { Confusion } from '../confusions';
 
 // TODO: Write test case
 export async function json(filename: string): Promise<Array<Confusion>> {
-    const response = await fetch(filename);
+    // TODO: should use https://kit.svelte.dev/docs/load or some other way to load data that does not involve using window.location.href
+    const response = await fetch(`${window.location.href}/${filename}`);
     const json = await response.json();
     return json.data.values;
 }
